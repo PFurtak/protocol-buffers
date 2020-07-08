28538e77112e4cbde4af264324a996e6ba573066
Patrick Furtak <patrick@patrickfurtak.com>
2020-07-08 15:27:30 -0400

    Wrote test code to work with the struct generated by the proto compiler

--- go/main.go
+++ go/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	simplepb "github.com/basics/go/src/simple"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	doSimple()
+}
+
+func doSimple() {
+	sm := simplepb.SimpleMessage{
+		Id:         12345,
+		IsSimple:   true,
+		Name:       "Simple Test",
+		SampleList: []int32{1, 2, 4, 8},
+	}
+
+	fmt.Println(sm)
+
+	sm.Name = "I renamed you"
+	fmt.Println(sm)
+
+	fmt.Println("The ID is: ", sm.Id)
 }
